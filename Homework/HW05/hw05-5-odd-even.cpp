892745090a0e609447dd03a5a204983cbe010e5f
JACK-Prime <natthakitrodruean@gmail.com>
2025-07-11 15:03:14 +0700

    upload HW05-5

---
+++ Homework/HW05/hw05-5-odd-even.cpp
@@ -0,0 +1,32 @@
+/*
+    จงรับตัวเลขจากผู้ใช้ และให้แสดงผลลัพธ์ว่าเป็นเลขคู่หรือเลขคี่
+    Test case:
+        Input :
+            15
+
+    Output:
+        15 is an odd integer
+
+    Test case:
+        Input :
+            -22
+
+    Output:
+        -22 is an even integer
+*/
+#include <stdio.h>
+int main () {
+    int num = 0 ;
+
+    printf("Enter number(check even or odd) : ") ;
+    scanf("%d",&num) ;
+    //process
+    if (num % 2 == 0) {
+        printf("%d is an even integer" ,num) ;
+    }
+    else{
+        printf("%d is an odd integer" ,num) ;
+    }
+
+    return 0 ;
+}
