4046dd02e1a706ae9d9a004ed1bd01a89d655d4c
JACK-Prime <natthakitrodruean@gmail.com>
2025-07-11 23:01:57 +0700

    this lap so confused

---
+++ LAP05/Lab05-If-Else-Switch-Case.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+int main () {
+    int score = 0 ;
+    int check ;
+
+    printf("Enter score : ") ;
+    check = scanf("%d", &score) ;  // printf("%d", check); false = 0 , true = 1  --> if input number = true(1) otherwise false(0) 
+
+
+    if (check == 0) {
+        printf("please enter number only.");
+    }
+    else if (score > 80) {
+        printf("A !\n");
+    }
+    else if (score >= 75 && score <= 80) {
+        printf("B+ !\n");
+    }
+    else if (score >= 70 && score < 75) {
+        printf("B !\n");
+    }
+    else if (score >= 65 && score < 70) {
+        printf("C+ !\n");
+    }
+    else if (score >= 60 && score < 65) {
+        printf("C !\n");
+    }
+    else if (score >= 55 && score < 60) {
+        printf("D+ !\n");
+    }
+    else if (score >= 50 && score < 55) {
+        printf("D !\n");
+    }
+    else { // score < 50
+        printf("F !\n");
+    }
+    
+
+    return 0 ;
+}
