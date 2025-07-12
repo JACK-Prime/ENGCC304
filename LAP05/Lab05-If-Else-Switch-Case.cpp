963ad14f18c54c04d8597d97edf78f99a7aab041
JACK-Prime <natthakitrodruean@gmail.com>
2025-07-12 19:47:21 +0700

    changes LAP05

--- LAP05/Lab05-If-Else-Switch-Case.cpp
+++ LAP05/Lab05-If-Else-Switch-Case.cpp
@@ -10,26 +10,29 @@ int main () {
     if (check == 0) {
         printf("please enter number only.");
     }
-    else if (score > 80) {
+    else if (score >= 80) {
         printf("A !\n");
     }
-    else if (score >= 75 && score <= 80) {
+    else if (score >= 75 && score < 80) { // 75 - 79
         printf("B+ !\n");
     }
-    else if (score >= 70 && score < 75) {
+    else if (score >= 70 && score < 75) { // 70 - 74
         printf("B !\n");
     }
-    else if (score >= 65 && score < 70) {
+    else if (score >= 65 && score < 70) { // 65 - 69
         printf("C+ !\n");
     }
-    else if (score >= 60 && score < 65) {
+    else if (score >= 60 && score < 65) { // 60 - 64
         printf("C !\n");
     }
-    else if (score >= 55 && score < 60) {
+    else if (score >= 55 && score < 60) { // 55 - 59
         printf("D+ !\n");
     }
-    else if (score >= 50 && score < 55) {
+    else if (score >= 50 && score < 55) { // 50 - 54
         printf("D !\n");
+    }
+    else if (score > 100 || score < 0){
+        printf("Invalid score");
     }
     else { // score < 50
         printf("F !\n");
