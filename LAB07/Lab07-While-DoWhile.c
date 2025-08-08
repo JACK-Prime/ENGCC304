2ed10962e0d46ed50f57de8c14e4fc1329d0f545
JACK-Prime <natthakitrodruean@gmail.com>
2025-08-09 01:13:55 +0700

    change LAB07(modified-7)

--- LAB07/Lab07-While-DoWhile.c
+++ LAB07/Lab07-While-DoWhile.c
@@ -3,12 +3,13 @@
 #include <time.h>
 #include <stdbool.h>
 
+void GUESS_play();
+
 int main () {
     srand( time( NULL ) ) ; //seed random
     int random_num ; 
     int guess_num = 0 ;
-    int check = 0 ;
-    int score , min , max ;
+    int score = 0 , max = 0 , min = 0 ;
     char ans[3] ;
     bool condition = true ;
     
@@ -17,50 +18,13 @@ int main () {
         printf("\nDo you want to play game (1=play,-1=exit) : \n") ;
         scanf("%s", ans) ; // check %d integer 
 
-        if ( ans[0] == '1') {
-            random_num = rand() % 100 + 1 ; // ถ้าไม่ใส่ + 1 จะนับตั้งแต่ 0 - 99  ---> ถ้าใส่ 1 - 100
+        if ( ans[0] == '1' && ans[1]== '\0') {
             score = 100 ;
-            min = 1 ;
-            max = 100 ;
-
-            printf ("\n(Score=%d)\n" , score ) ;
-            do{
-                printf("\nGuess the winning number (%d-%d) :\n", min , max ) ;
-                check = scanf("%d", &guess_num ) ; // check %d integer only 
-
-                if (check != 1) {
-                    printf("\nPlease, enter number between %d - %d \n", min, max) ;
-                    while(getchar() != '\n') ;//clear buffer เช่น abd\n เจอ \n ออกลูป ----เอาไว้กันเผื่อ user input ที่ไม่ใช่ %d
-                }//end if 
-                else if (guess_num < min || guess_num > max) {
-                    printf("\nPlease, enter number between %d - %d \n", min, max) ;
-                }//end else if 
-                else if (guess_num == random_num) {
-                    printf("\nThat is correct! The winning number is %d.\n", random_num) ;
-                    printf("\nScore this game: %d\n", score) ;
-                }//end else if 
-                else if (guess_num > random_num) {
-                    score -= 10 ;
-                    if (score <= 0) {
-                        printf("\nGAME OVER >>>> your score is %d.\n", score) ;
-                        break;
-                    }//end if 
-                    printf("\nSorry, the winning number is LOWER than %d. (Score=%d)\n", guess_num, score);
-                    max = guess_num - 1 ;
-                }//end else if
-                else {
-                    score -= 10 ;
-                    if (score <= 0) {
-                        printf("\nGAME OVER >>>> your score is %d.\n", score) ;
-                        break;
-                    }//end if 
-                    printf("\nSorry, the winning number is HIGHER than %d. (Score=%d)\n", guess_num, score);
-                    min = guess_num + 1 ;
-                }//end else 
-            } while ( guess_num != random_num) ;//end do-while
+            printf("\n(Score=%d)\n" , score ) ;
+            GUESS_play();
         
         }//end if
-        else if ( ans[0] == '-' && ans[1] == '1') {
+        else if ( ans[0] == '-' && ans[1] == '1' && ans[2]== '\0') {
             printf("\nSee you again.\n") ;
             condition = false ;
         }//end else if
@@ -69,4 +33,48 @@ int main () {
         }//end else 
     }//end while
     return 0 ;
+}
+
+void GUESS_play() {
+    int guess_num ;
+    int check = 1 ;
+    int score = 100 ;
+    int max = 100  ;
+    int min = 1  ;
+    int random_num = rand() % 100 + 1 ; // ถ้าไม่ใส่ + 1 จะนับตั้งแต่ 0 - 99  ---> ถ้าใส่ 1 - 100
+    do{
+        printf("\nGuess the winning number (%d-%d) :\n", min , max ) ;
+        check = scanf("%d", &guess_num ) ; // check %d integer only 
+
+        if (check != 1) {
+            printf("\nPlease, enter number between %d - %d \n", min, max) ;
+             while(getchar() != '\n') ;//clear buffer เช่น abd\n เจอ \n ออกลูป ----เอาไว้กันเผื่อ user input ที่ไม่ใช่ %d
+        }//end if 
+        else if (guess_num < min || guess_num > max) {
+            printf("\nPlease, enter number between %d - %d \n", min, max) ;
+        }//end else if 
+        else if (guess_num == random_num) {
+            printf("\nThat is correct! The winning number is %d.\n", random_num) ;
+            printf("\nScore this game: %d\n", score) ;
+            break;
+        }//end else if 
+        else if (guess_num > random_num) {
+            score -= 10 ;
+            if (score <= 0) {
+                printf("\nGAME OVER >>>> your score is %d.\n", score) ;
+                break;
+            }//end if 
+            printf("\nSorry, the winning number is LOWER than %d. (Score=%d)\n", guess_num, score);
+            max = guess_num - 1 ;
+        }//end else if
+        else {
+            score -= 10 ;
+            if (score <= 0) {
+                printf("\nGAME OVER >>>> your score is %d.\n", score) ;
+                break;
+            }//end if 
+            printf("\nSorry, the winning number is HIGHER than %d. (Score=%d)\n", guess_num, score);
+            min = guess_num + 1 ;
+        }//end else
+    } while (true) ;
 }
