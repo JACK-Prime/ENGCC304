0f504bdb4490506c26126110fd83c959e86b0c72
JACK-Prime <natthakitrodruean@gmail.com>
2025-08-18 20:03:47 +0700

    upload HW08-1

---
+++ Homework/HW08/hw08-1-basic-display.c
@@ -0,0 +1,42 @@
+/*เขียนโปรแกรมรับตัวเลขจากผู้ใช้ n ชุด จากนั้นทำการหาผลรวม และค่าเฉลี่ยของชุดตัวเลขเหล่านั้น (แสดงค่าเฉลี่ยเป็นทศนิยม 2 ตำแหน่ง)
+    
+    Test case:
+        Enter value n :
+            6
+        Enter number 1 :
+            19
+        Enter number 2 :
+            7
+        Enter number 3 :
+            23
+        Enter number 4 :
+            96
+        Enter number 5 :
+            77
+        Enter number 6 :
+            16
+    Output:
+        Sum = 238
+        Avg = 39.67  */
+#include<stdio.h>
+
+int main () {
+    int n = 0  ; 
+    float avg = 0.0f ,sum = 0.0f , numbers = 0.0f ; 
+
+    printf("Enter value n : \n") ;
+    scanf("%d" , &n );
+
+    for (int i = 1 ; i <= n ; i++ ) {
+        printf("Enter number %d\n",i) ;
+        scanf("%f" , &numbers ) ;
+        sum += numbers ;
+    }//end for 
+
+    avg = sum / n ;
+    printf("\nSum = %.0f\n", sum) ;
+    printf("Avg = %.2f", avg) ;
+
+    return 0 ;
+
+}
