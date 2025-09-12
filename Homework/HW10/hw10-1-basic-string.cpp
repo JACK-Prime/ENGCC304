bbb85348d8f3bfcfc83056e4c3c92a413625cfeb
JACK-Prime <natthakitrodruean@gmail.com>
2025-09-12 13:56:48 +0700

    upload HW10-1

---
+++ Homework/HW10/hw10-1-basic-string.cpp
@@ -0,0 +1,27 @@
+/* จงเขียนโปรแกรมเพื่อรับค่า String ที่สามารถรับชื่อและนามสกุลไว้ในตัวแปรเดียวกันได้ พร้อมกับแสดงผลบนหน้าจอคอมพิวเตอร์
+    Test case:
+        Kittinan Noimanee
+    Output:
+        Name: Kittinan Noimanee
+
+    Test case:
+        Visual Studio Code
+    Output:
+        Name: Visual Studio Code
+*/
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char name[ 50 ] ;
+
+    printf("Input character : ") ;
+    fgets(name, sizeof(name), stdin) ;// มี \n อยู่ใน name หลังจาก fgets
+    getchar() ;
+    printf("Name: %s\n", name) ;
+
+    return 0 ;
+}
+
+
+
