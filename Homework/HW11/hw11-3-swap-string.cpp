fdd440e1163135b73bd8c2d8366899ebbbe65394
JACK-Prime <natthakitrodruean@gmail.com>
2025-09-13 11:35:23 +0700

    upload HW11-3

---
+++ Homework/HW11/hw11-3-swap-string.cpp
@@ -0,0 +1,40 @@
+/* ผู้ใช้พิมพ์ข้อความเข้ามาในระบบ และให้คุณทำการแสดงการกลับตัวอักษรทั้งหมดที่ผู้ใช้กรอกมา เช่น ผู้ใช้กรอกมาว่า Hello World คุณจะต้องแสดงบนหน้าจอว่า dlroW olleH
+    Test case:
+        Hello world
+    Output:
+        dlrow olleH
+
+    Test case:
+        RMUTL
+    Output:
+        LTUMR
+
+    Test case:
+        Orio
+    Output:
+        oirO
+*/
+#include <stdio.h>
+#include <string.h>
+
+void swapSTR(char *, char *) ;
+
+int main () {
+    char words[30] ;
+    char reverse[30] ;
+
+    printf("Enter words : ") ;
+    scanf("%s" ,words) ;
+
+    printf("\n** reverse **") ;
+    swapSTR(words , reverse ) ; 
+    printf("\n%s" , reverse) ;
+
+}
+
+void swapSTR(char *in , char *out) {
+    for (int i = 0 ; i < strlen(in) ; i++) {
+        char reverse[30] ;
+        out[ i ] = in[ (strlen(in) - 1) - i ] ;
+    }//end for 
+}
