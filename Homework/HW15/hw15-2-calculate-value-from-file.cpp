32f979398f11cbba543286af12f1818450bb828f
JACK-Prime <natthakitrodruean@gmail.com>
2025-10-03 23:27:28 +0700

    upload HW15

--- Homework/HW15/hw15-2-calculate-value-from-file.cpp
+++ Homework/HW15/hw15-2-calculate-value-from-file.cpp
@@ -16,3 +16,30 @@
         Input value from user is 20
         Answer = 22
 */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main () {
+    int num_user = 0 ;
+    printf("Input vale from user to B: ") ;
+    scanf("%d" , &num_user ) ;
+
+    FILE *fp = fopen("a15-2.txt" , "r" ) ;
+    if (fp == NULL) {
+        printf("\nError opening file.") ;
+        exit( 0 ) ;
+    }//end if 
+
+    int num_file ;
+    
+    // Read data of file in specific format
+    while (fscanf(fp, "%d", &num_file) != EOF) ;
+
+    fclose(fp) ;
+    printf("\nRead File to A is %d\n", num_file) ;
+    printf("Input value from user is %d\n" , num_user ) ;
+    printf("Answer = %d\n" , num_file + num_user) ; 
+
+    return 0 ; 
+}
